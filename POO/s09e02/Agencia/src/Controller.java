ed6f087c058e136406883db4b504b91a2c5e20a5
Times <deyvidson98@gmail.com>
2019-10-10 21:55:52 -0300

    up

--- POO/s09e02/Agencia/src/Controller.java
+++ POO/s09e02/Agencia/src/Controller.java
@@ -1,157 +1,220 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-import com.sun.org.apache.xalan.internal.xsltc.compiler.sym;
+class Cliente {
 
-class Cliente{
-	protected String nome;
-	ArrayList<Conta> contas;
-	
-	public Cliente(String nome) {
-		this.nome = nome;
-		
-	}
+    private String nome;
+    private ArrayList<Conta> contas;
 
-	public String getNome() {
-		return nome;
-	}
+    Cliente(String nome) {
+        this.nome = nome;
+        contas = new ArrayList<Conta>();
+    }
 
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void addConta(Conta conta) {
+        this.contas.add(conta);
+
+    }
 }
-class Conta{
-	protected int id;
-	protected String nome_cliente;
-	protected String tipo;
-	protected double saldo;
-	
-	public Conta(int id, String nome_cliente, double saldo, String tipo) {
-		this.id = id;
-		this.nome_cliente = nome_cliente;
-		this.saldo = saldo;
-		this.tipo = tipo;
-	}
-	public Conta(){
+abstract class Conta {
+    static int aux = 0; 
+    private int id = 0;
+    private String nome;
+    private double saldo;
+    private String tipo;
 
-	}
+    Conta(String nome, double saldo, String tipo) {
+        this.nome = nome;
+        this.saldo = saldo;
+        this.tipo = tipo;
+    }
 
-	public int getId() {
-		return id;
-	}
+    Conta() {
+    }
+    
+    public int getId() {
+        return id;
+    }
 
-	public void setId(int id) {
-		this.id = id;
-	}
+    public void setId(int id) {
+        this.id = id;
+    }
 
-	public String getNome_cliente() {
-		return nome_cliente;
-	}
+    public static int getAux() {
+        return aux;
+    }
 
-	public void setNome_cliente(String nome_cliente) {
-		this.nome_cliente = nome_cliente;
-	}
+    public static void setAux(int aux) {
+        Conta.aux = aux;
+    }
 
-	public String getTipo() {
-		return tipo;
-	}
+    public String getNome() {
+        return nome;
+    }
 
-	public void setTipo(String tipo) {
-		this.tipo = tipo;
-	}
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 
-	public double getSaldo() {
-		return saldo;
-	}
+    public double getSaldo() {
+        return saldo;
+    }
 
-	public void setSaldo(double saldo) {
-		this.saldo = saldo;
-	}
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
 }
-class Corrente extends Conta{	
-	public Corrente(String nome_cliente) {
-		this.setId(id);
-		this.setNome_cliente(nome_cliente);
-		this.setSaldo(saldo);
-		this.setTipo("CC");
-	}
+class Corrente extends Conta {
+
+    public Corrente(String nome) {
+        this.setId(aux);
+        this.setNome(nome);
+        this.setSaldo(getSaldo());
+        this.setTipo("CC");
+        aux ++;
+    }
 }
-class Poupanca extends Conta{
+class Poupanca extends Conta {
 
-	public Poupanca(String nome_cliente) {
-		this.setId(id);
-		this.setNome_cliente(nome_cliente);
-		this.setSaldo(saldo);
-		this.setTipo("CP");
+    public Poupanca(String nome) {
+        this.setId(aux);
+        this.setNome(nome);
+        this.setSaldo(getSaldo());
+        this.setTipo("CP");
+        aux ++;
 
-	}
+    }
+
 }
-class Agencia{
-	ArrayList<Cliente> clientes;
-	ArrayList<Conta> contas;
+class Agencia {
 
-	public Agencia() {
-		clientes = new ArrayList<Cliente>();
-		contas = new ArrayList<Conta>();
-	}
-	public void addCli(String nome){
-		for(Cliente x : clientes){
-			if(x.getNome().equals(nome)){
-				System.out.println("Cliente já existe!");
-				return;
-			}
-		Cliente cliente = new Cliente(nome);
-		Conta poupanca = new Conta(nome);
-		Conta corrente = new Conta(nome);
-		cliente.add(poupanca);
-		cliente.add(corrente);
-		this.contas.add(poupanca);
-		this.contas.add(corrente);
-		}
-	}
-	public void sacar(int indice, double valor) {
-		for(Conta x : contas){
-			if(indice == c.getId()){
-				if(valor <= this.saldo) {
-				this.saldo -= valor;
-				System.out.println("Operação realizada com sucesso!");
-				System.out.println("Saldo atual: " + c.getSaldo());
-				}else {
-				System.out.println("Valor incompatível!");
-				}
-			}
-		}
-	}public void despositar(int indice, double valor) {
-		for(Conta x : contas){
-			if(indice == c.getId()){
-				this.saldo += valor;
-				System.out.println("Operação realizada com sucesso!");
-				System.out.println("Saldo atual: " + c.getSaldo());
-			}
-		}
-	}
-	public void update(){
-		for(Conta x : contas){
-			if(c.getTipo().equals("CP")){
-				
-			}
-		}
-	}
+    private ArrayList<Cliente> clientes;
+    private ArrayList<Conta> contas;
+
+    Agencia() {
+        clientes = new ArrayList<Cliente>();
+        contas = new ArrayList<Conta>();
+
+    }
+
+    public void addCli(String nome) {
+        for (Cliente x : clientes) {
+            if (x.getNome().equals(nome)) {
+                System.out.println("Cliente já existe");
+                return;
+            }
+        }
+        Cliente cliente = new Cliente(nome);
+        Conta corrente = new Corrente(nome);
+        Conta poupanca = new Poupanca(nome);
+        this.clientes.add(cliente);
+        cliente.addConta(corrente);
+        cliente.addConta(poupanca);
+        this.contas.add(corrente);
+        this.contas.add(poupanca);
+    }
+    
+    public void show() {
+        for (Conta x : contas) {
+            System.out.println(x.getId() + ":" + x.getNome() + ":" + x.getSaldo() + ":" + x.getTipo());
+        }
+    }
+    public void depositar(int indice, double valor){
+        for (Conta x : contas){
+            if(indice == x.getId()){
+                x.setSaldo(x.getSaldo()+valor);
+                return;
+            }
+        }
+        System.out.println("Conta nem existe, parsa");
+    }
+    public boolean sacar(int indice, double valor){
+        for (Conta x : contas){
+            if(indice == x.getId()){
+                if(valor <= x.getSaldo()){
+                    x.setSaldo(x.getSaldo()-valor);
+                    return true;
+                }
+            }
+            else if(indice == x.getId()){
+                if(valor <= x.getSaldo()){
+                    System.out.println("Valor alto demais");
+                    return false;
+                }
+            }
+        }
+        System.out.println("Conta nem existe, man");
+        return false;
+    }
+    public void transferir(int i1, int i2, double valor){
+        if(this.sacar(i1, valor)){
+            this.depositar(i2, valor);
+        }
+    }
+    public void update(){
+        for (Conta x : contas){
+            if(x.getTipo().equals("CC")){
+                x.setSaldo(x.getSaldo()-20);
+            }
+            else{
+                x.setSaldo(x.getSaldo()+(x.getSaldo()*0.01));
+            }
+        }
+    }
+    
 }
-
 
 public class Controller {
 
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-
-		while(true) {
-			System.out.println("aadCli, sacar, depositar, transferir, show, end");
-			System.out.println("add,");
-			String line = scan.nextLine();
-			String[] vet = line.split(" ");
-			break;
-		}
-	}
-
+    public static void main(String[] args) {
+        Agencia agencia = new Agencia();
+        Scanner scan = new Scanner(System.in);
+        while (true) {
+            System.out.println("addCli, sacar, depositar, transferir, update, show, end");
+            String line = scan.nextLine();
+            String vet[] = line.split(" ");
+                
+            if(vet[0].equals("addCli")) {
+                agencia.addCli(vet[1]);
+            }
+            else if(vet[0].equals("depositar")) {
+                agencia.depositar(Integer.parseInt(vet[1]), Double.parseDouble(vet[2]));
+            }
+            else if(vet[0].equals("sacar")) {
+                agencia.sacar(Integer.parseInt(vet[1]), Double.parseDouble(vet[2]));
+            }
+            else if(vet[0].equals("transferir")){
+                agencia.transferir(Integer.parseInt(vet[1]), Integer.parseInt(vet[2]), Double.parseDouble(vet[3]));
+            }
+            else if(vet[0].equals("show")) {
+                agencia.show();
+            }
+            else if(vet[0].equals("end")) {
+                break;
+            }
+            else if(vet[0].equals("update")) {
+                agencia.update();
+            }
+            else{
+                System.out.println("Comando inválido!");
+            }
+        }
+    }
 }
